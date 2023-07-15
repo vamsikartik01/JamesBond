bd52f138bdbe05eeaa495fa42dc66a28621c2468
vamsikartik01 <vamsikartik01@gmail.com>
2023-07-15 09:46:36 +0530

    added Right bar info panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BodyComponent } from './body/body.component';
+import { RightSideBarComponent } from './right-side-bar/right-side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     NavBarComponent,
-    BodyComponent
+    BodyComponent,
+    RightSideBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/right-side-bar/right-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-right-side-bar',
+  templateUrl: './right-side-bar.component.html',
+  styleUrls: ['./right-side-bar.component.css']
+})
+export class RightSideBarComponent {
+  @Input() enableRightbarButton: boolean = false;
+  @Output() toggleRight = new EventEmitter<boolean>()
+
+  toggleRightbar(): void{
+    this.toggleRight.emit(true);
+  }
+}
